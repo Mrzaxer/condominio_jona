9cc4f6ece96bae602fdca71b8a3dd9b9318ad40a
Mrzaxer <efrainlars209@gmail.com>
2025-02-24 23:04:12 -0600

    sesiones2

--- src/configuracion.jsx
+++ src/configuracion.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './Configuracion.css';
+import './configuracion.css';
 
 const Configuracion = () => {
   const navigate = useNavigate();
